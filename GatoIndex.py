77d4cc97adc8c7a72c9131e60e637aeaa8a85a84
NicolasAraya932 <nicoarayapcap@gmail.com>
2022-06-24 23:48:54 -0400

    Update GatoIndex.py
    Tu mama

--- GatoIndex.py
+++ GatoIndex.py
@@ -20,6 +20,7 @@ class Ventana(QtWidgets.QMainWindow):
         self._cambiarsymbol = "x"
         # Triggers
         x = "hola"
+        qiwndoiqwnodinqw = 123
 
         self.ui.confirm.clicked.connect(self.changeSymbol)
 
